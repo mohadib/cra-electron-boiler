605ab73e8ac6756b9a758b920ab27b8f3f83c1fe
Jason Davis <jdavis@openactive.org>
2020-03-21 22:47:25 -0600

    fix broken dep, add fs example

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import {MuiThemeProvider} from '@material-ui/core/styles'
 import { theme } from './MuiTheme'
 
 class App extends Component {
+  state = {
+    files : []
+  }
+
+  componentDidMount() {
+    const f = window.require('fs')
+    this.setState({ files: f.readdirSync('/')})
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <div>Hello World</div>
+        <ul>
+          { this.state.files.map( f => (
+            <li>{f}</li>
+          ))}
+        </ul>
       </MuiThemeProvider>
     )
   }
